4f986a8ce139ac5ab4c09245ab8228078a69a164
Mathew Jose Matanguihan <mathewjose.matanguihan@tup.edu.ph>
2022-03-12 23:22:45 +0800

    latest

--- app/src/main/java/com/example/mobile_aris/update_profile.java
+++ app/src/main/java/com/example/mobile_aris/update_profile.java
@@ -3,10 +3,12 @@ package com.example.mobile_aris;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -18,8 +20,17 @@ public class update_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+        Button btn_uploadUserProfile;
+        btn_uploadUserProfile = findViewById(R.id.btn_uploadUserProfile);
+        btn_uploadUserProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
     }
-  
+
 }
 
 
